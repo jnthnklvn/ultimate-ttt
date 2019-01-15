faae35cd7964ed003724bc7f1addb664473d58bc
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-01-15 19:12:00 -0300

    codigo do professor

---
+++ jogo-da-velha.py
@@ -0,0 +1,84 @@
+class TicTacToe(Game):
+    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
+    A state has the player to move, a cached utility, a list of moves in
+    the form of a list of (x, y) positions, and a board, in the form of
+    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+
+    def __init__(self, h=3, v=3, k=3):
+        self.h = h
+        self.v = v
+        self.k = k
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                             utility=self.compute_utility(state.board, move, state.to_move),
+                             board=state.board, moves=state.moves)
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility  
+    
+    def eval_function(self, state, player):
+            v = 0            
+            oponent = 'O' if player == 'X' else 'X'
+            
+            for x in range(1, 4):
+                for y in range(1, 4):
+                    if state.board.get((x, y)) == player:
+                        v += 2 if ((x+y) % 2) == 1 else 3 if (not (x == 2)) else 4
+            for x in range(1, 4):
+                for y in range(1, 4):
+                    if state.board.get((x, y)) == oponent:
+                        v -= 2 if ((x+y) % 2) == 1 else 3 if (not (x == 2)) else 4
+                        
+            u = self.utility(state, player)
+            return u if not u == 0 else v  
+
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+        print()    
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +20 if player == 'X' else -20
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
